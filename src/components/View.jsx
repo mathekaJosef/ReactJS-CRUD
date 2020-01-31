51678837a8c3f3e0fc0469495832c945d1b8a2d7
mathekaJosef <mathekajosef571@gmail.com>
2020-01-31 23:49:17 +0300

    work on view component

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -4,7 +4,8 @@ export class View extends Component {
     render() {
         return (
             <div>
-                <p>Welcome to insert view component!!</p>
+                <h3>Welcome to insert view component!!</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis excepturi veniam aspernatur eaque praesentium doloribus odio soluta perspiciatis, itaque commodi neque quo laborum. Quas, laudantium commodi voluptatum et adipisci unde.</p>
             </div>
         )
     }
